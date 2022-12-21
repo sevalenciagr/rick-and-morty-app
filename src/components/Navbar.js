a7dab2ddd4b439e1175f968f4135f8c4aee76d15
sevalenciagr <sevalenciagr@gmail.com>
2022-12-20 23:01:11 -0500

    Creando una aplicacion para manipulacion API

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand text-uppercase" href="/">Rick and Morty App</a>
+      </div>
+    </nav>
+  )
+}
+
